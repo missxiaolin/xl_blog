7a8656f379f0bd87abcf4c04f43df075f39860fe
xiaolin <462441355@qq.com>
2021-11-21 19:09:28 +0800

    * config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  theme: 'vdoing',
   title: 'Chen\'s blog',
   description: '我的个人网站',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
@@ -12,7 +13,7 @@ module.exports = {
     nav: [ // 导航栏配置
       { text: '前端基础', link: '/accumulate/' },
       { text: '算法题库', link: '/algorithm/' },
-      { text: '微博', link: 'https://baidu.com' }
+      { text: 'GitHub', link: 'https://baidu.com' }
     ],
     sidebar: 'auto', // 侧边栏配置
     sidebarDepth: 2, // 侧边栏显示2级
