e6f893d38d71b56b80d0988452208cfaa4a89176
xiaolin <462441355@qq.com>
2022-06-06 15:39:05 +0800

    * 配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     base: '/xl_blog/', // 这是部署到github相关的配置
     theme: 'vdoing',
-    title: 'Chen\'s blog',
+    title: 'xiaolin',
     description: '我的个人网站',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
         ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
